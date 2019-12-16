df5fe48d86f5513177a0b1d82f5a26e5f12aa782
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-12-17 00:24:47 +0900

    Yes No Button Window

--- Program.cs
+++ Program.cs
@@ -6,8 +6,28 @@ class Program
     [STAThread]
     static void Main()
     {
-        Form form = new Form();
-        form.Text = "Hello!";
-        Application.Run(form);
+        //===============================
+        // showメソッドについて
+        // static DialogResult Show(
+        // string text,                // メッセージ
+        // string caption,             // タイトル
+        // MessageBoxButtons buttons,  // ボタン
+        // MessageBoxIcon icon         // アイコン
+        // )
+        //===============================
+        DialogResult result = MessageBox.Show(
+            "[はい] か [いいえ] を選択してください。", "確認",
+            // ボタンの設定
+            MessageBoxButtons.YesNo,
+            // アイコンの設定
+            MessageBoxIcon.Question);
+        if (result == DialogResult.Yes)
+        {
+            MessageBox.Show("[はい] が選択されました。", "結果");
+        }
+        else if (result == DialogResult.No)
+        {
+            MessageBox.Show("[いいえ] が選択されました。", "結果");
+        }
     }
 }
