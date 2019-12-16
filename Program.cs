4c1f8ba7be29be4afe85a94d7ba73f38d6f0ebdc
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-12-17 00:16:28 +0900

    First Commit - Hello Window

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Form form = new Form();
+        form.Text = "Hello!";
+        Application.Run(form);
+    }
+}
