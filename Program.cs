5528357a2ec0cbb1783c9c81477d4a5cefc2a80f
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-12-23 22:36:02 +0900

    機能しないボタンを設置

--- Program.cs
+++ Program.cs
@@ -4,78 +4,24 @@ using System.Windows.Forms;
 
 class Program
 {
-    [STAThread]
-    static void Main()
-    {
-        //===============================
-        // showメソッドについて
-        // static DialogResult Show(
-        // string text,                // メッセージ
-        // string caption,             // タイトル
-        // MessageBoxButtons buttons,  // ボタン
-        // MessageBoxIcon icon         // アイコン
-        // )
-        //===============================
-        /*
-        DialogResult result = MessageBox.Show(
-            "[はい] か [いいえ] を選択してください。", "確認",
-            // ボタンの設定
-            MessageBoxButtons.YesNo,
-            // アイコンの設定
-            MessageBoxIcon.Question);
-        if (result == DialogResult.Yes)
-        {
-            MessageBox.Show("[はい] が選択されました。", "結果");
-        }
-        else if (result == DialogResult.No)
-        {
-            MessageBox.Show("[いいえ] が選択されました。", "結果");
-        }
-        */
-        Application.Run(new Form1());
-    }
+	[STAThread]
+	static void Main()
+	{
+		Application.EnableVisualStyles(); // ビジュアルスタイルを有効化
+		Application.Run(new Form1());
+	}
 }
 
 class Form1 : Form
 {
-    Label label = new Label(); // ラベルコントロール
+	Button button = new Button(); // ボタンコントロール
 
-    // コンストラクタ
-    public Form1()
-    {
-        // this.Text = "Hello!";
-        this.Text = "Title"; // ウィンドウタイトル設定
-        /*
-        bool ControlBox	コントロールボックスを表示
-        bool MaximizeBox	最大化ボタンを表示
-        bool MinimizeBox	最小化ボタンを表示
-        bool TopMost	最前面に表示
-        */
-        this.MaximizeBox = false; // 最大化ボタンを無効
-        /*
-         Color BackColor	背景色
-         Color ForeColor	前景色
-        */
-        this.BackColor = SystemColors.Window; // 背景色の設定
-        // this.BackColor = Color.Blue;
-        this.ClientSize = new Size(800, 800); // クライアント領域のサイズ
-        
-        // ウィンドウの出現位置
-        this.StartPosition = FormStartPosition.Manual; // 位置の決定方法
-        this.Location = new Point(100, 50); // ウィンドウの位置
+	public Form1()
+	{
+		button.Text = "OK";
+		button.Location = new Point(10, 10);
+		button.Size = new Size(120, 40);
 
-        // 背景画像の設定
-        // bin/Debug/**.exeからの相対パス
-        Bitmap bitmap = new Bitmap(@"..\..\testImage\uma.png");
-
-        this.BackgroundImage = bitmap; // 背景画像
-        this.BackgroundImageLayout = ImageLayout.Tile; // 配置
-
-        label.Text = "Hello, world!"; // テキスト
-        label.Location = new Point(10, 10); // 位置の指定
-        label.AutoSize = true; // 自動サイズ調整
-
-        this.Controls.Add(label); // コントロールをフォームに貼り付け
-        // Or label.Parent = this;
-    }
+		this.Controls.Add(button); // コントロールをフォームに貼り付け
+	}
 }
